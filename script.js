37c7601fcfa5fdc7c53f328b72b822b462efa012
warshoo <fsmf@yandex.by>
2022-09-12 01:10:50 +0300

    added names for questions

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const mainDiv = document.createElement("div");
-mainDiv.id = "main";
-document.body.appendChild(mainDiv);
+const mainDiv = document.querySelector('#main');
+let plan_items = [["question #1", 30], ["question #2", 20], ["question #3", 15], ["question #4", 25]];   // заглушка - исходные данные
+let number_of_items = plan_items.length;
 
-const button = document.createElement('button');
-button.innerText = 'Button';
-button.id = 'button-1';
-mainDiv.appendChild(button);
+
+for(let i = 0; i < number_of_items; i ++){
+    let current_theme = plan_items[i][0];
+    const itemDiv = document.createElement('div');
+    itemDiv.innerText = `${current_theme}`;
+    itemDiv.className = 'itemDiv';
+    mainDiv.appendChild(itemDiv);
+}
+
